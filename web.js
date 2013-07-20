a5a01210db8fe3d9851cbc34452e211e394456a6
Ubuntu <ubuntu@ip-10-245-137-113.us-west-2.compute.internal>
2013-07-20 07:32:38 +0000

    modified web.js for errors

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var content = fs.readFileSync('./index.html','utf-8');
+var content = fs.readFileSync('index.html','utf-8');
 app.get('/', function(request, response) {
   response.send(content);
 });
